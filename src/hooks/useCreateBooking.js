7c79b7f9666ab104ac14a3aaf95e68d0ecedf6cc
Ammar Afridi <ammar.afridi95@gmail.com>
2025-11-03 15:41:37 +0400

    Create booking functionality added

---
+++ src/hooks/useCreateBooking.js
@@ -0,0 +1,26 @@
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
+import { createBookingApi, getPaymentLinkApi } from '../services/apiBooking';
+
+export function useCreateBooking() {
+  const createBooking = useMutation({
+    mutationFn: async (formData) => {
+      const booking = await createBookingApi(formData);
+      if (!booking?._id) throw new Error('Booking creation failed');
+
+      const paymentData = await getPaymentLinkApi(booking._id);
+
+      if (!paymentData) throw new Error('Failed to retrieve payment link');
+
+      window.location.href = paymentData;
+    },
+    onError: (err) => {
+      toast.error(err.message || 'Booking failed');
+    },
+  });
+
+  return {
+    createBooking: createBooking.mutate,
+    isCreating: createBooking.isPending,
+  };
+}
